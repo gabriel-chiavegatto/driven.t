7c3c81f2a65cba905b2ae6a3a87759640abfd7be
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2023-05-02 17:58:59 -0300

    tests no token POST/booking

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -82,6 +82,28 @@ describe('GET /booking', () => {
     });
   });
 });
-// describe('GET /booking', () => {
+describe('POST /booking', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.post('/booking');
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
 
-// })
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.post('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post('/booking').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  // describe('when token is valid', () => { })
+});
